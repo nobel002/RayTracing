010fbeaa03eb2f36b4880be6db76ef1fa4c13a45
nobel002 <43477230+nobel002@users.noreply.github.com>
2021-02-16 17:37:01 +0100

    see fuction got an update

--- particle.py
+++ particle.py
@@ -1,5 +1,6 @@
 from line import Line
 import math
+from math import sin, cos
 
 
 class Particle(Object):
@@ -8,13 +9,20 @@ class Particle(Object):
         self.heading = heading  # should be an agle in radians
         self.fov = fov  # should be in radians
         self.numOfRays = numOfRays  # should ba an int
+        self.deltaAngle = fov/numOfRays
 
     def see(self, enviroment):
         lineOfSight = []
-        oldLine = Line(0,0,0,2048)
+        oldLine = Line(0, 0, 0, 2048)
+        oldLine.fromTwoPoints(self.pos, (self.pos[0] + cos(self.heading), self.pos[1] + sin(self.heading)))
         for line in range(fov):
            closes = math.inf
-           
+           closestIntersectPoint = oldLine
+           relativeIndex = fov/2 - line 
+           oldLine.rotateLine(relativeIndex-self.deltaAngle)
            for item in enviroment:
-               Line(0, 0, 0, 2048).rotateLine()
+               if oldLine.calcIntersect(item)["distance"] <= closes:
+                    closes = oldLine.calcIntersect(item)["distance"]
+                    closestIntersectPoint = oldLine.calcIntersect(item)["intersect"]
+           lineOfSight.append(closestIntersectPoint)
         return lineOfSight
